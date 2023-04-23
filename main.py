a4a8741037a97f7e20c5bb32dd416d50084d8b04
mohamadbastin <mohamadbastin@gmail.com>
2023-04-23 14:38:18 +0330

    change to main

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def report():
         t += " "*4
 
         # contract balance
-        t += f"""C: {str(chain['contractBalance'] / 10**18)[:4]} {chain['symbol']} {tick if chain["hasEnoughFunds"] is True else cross}"""
+        t += f"""C: {str(chain['contractBalance'] / 10**int(chain["decimals"]))[:5]} {chain['symbol']} {tick if chain["hasEnoughFunds"] is True else cross}"""
 
         # 4 spaces
         t += " "*4
