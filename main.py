31d22ba2737eb954d488a3cdbe29edce1d8f88ea
mohamadbastin <mohamadbastin@gmail.com>
2023-04-23 12:18:33 +0330

    tmp

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import schedule
 from feeder import get_all_chain_balances
 from bot import send_message
 
+links = {}
+
 
 def report():
     data = get_all_chain_balances()
-    send_message(data)
+    text = "Chain Balances:\n"
+    # for chain in data:
+    t = """Chain: Fantom 
+Type: EVM
+
+Contract Address: 6284758451:AAELuZzT9h2ZCubQ_LxWwTPoqYFe3lR9CsI
+
+Wallet Address: 0xB10f8E218A9cD738b0F1E2f7169Aa3c0897F2d83"""
+    send_message(str(data))
 
 
 report()
