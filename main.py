f07849e2f75683c52ea6a85351101b0db230321d
mohamadbastin <mohamadbastin@gmail.com>
2023-04-23 12:47:46 +0330

    tmp

--- main.py
+++ main.py
@@ -35,7 +35,43 @@ Wallet Address: `{chain["wallet"]}`"""
     pin_message(resp["result"]["message_id"])
 
 
-set_linked_and_pinned()
+green_circle = "\U0001F7E2"
+red_circle = "\U0001F534"
+tick = "\u2714"
+cross = "\u274C"
+
+
+def report():
+    data = get_all_chain_balances()
+    t = ""
+    for chain in data:
+        # circle color
+        if chain["needsFunding"] == "true":
+            t += red_circle
+        elif chain["needsFunding"] == "false":
+            t += green_circle
+
+        # space
+        t += " "
+
+        # chain name
+        t += f"{chain['chainName']}:"
+
+        # 4 spaces
+        t += " "*4
+
+        # contract balance
+        t += f"""C: {(chain['contractBalance'] / 10**18)[:4]} {chain['symbol']} {tick if chain["hasEnoughFunds"] == "true" else cross}"""
+
+        # 4 spaces
+        t += " "*4
+
+        # wallet balance
+        t += f"""W: {(chain['walletBalance'] / 10**18)[:4]} {chain['symbol']} {tick if chain["hasEnoughFees"] == "true" else cross}"""
+
+
+# set_linked_and_pinned()
+report()
 
 # schedule the function to run every 6 hours
 # schedule.every(6).hours.do()
